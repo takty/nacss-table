d1aad21694fc654806c3c94081ac81b35a418d30
Takuto Yanagida <yanagida@stxst.com>
2022-03-23 14:17:24 +0900

    Replace and update packages

--- gulpfile.js/task-timestamp.js
+++ gulpfile.js/task-timestamp.js
@@ -3,22 +3,24 @@
  * Function for gulp (Timestamp)
  *
  * @author Takuto Yanagida
- * @version 2021-10-14
+ * @version 2022-03-23
  *
  */
 
 'use strict';
 
 const gulp = require('gulp');
-const $    = require('gulp-load-plugins')({ pattern: ['gulp-*'] });
+const $    = require('gulp-load-plugins')({ pattern: ['gulp-plumber', 'gulp-ignore', 'gulp-replace', 'gulp-changed'] });
+
+const require_ = (path) => { let r; return () => { return r || (r = require(path)); }; }
 
 function makeTimestampTask(src, dest = './dist', base = null) {
-	const require_ = (path) => { let r; return () => { return r || (r = require(path)); }; }
-	const moment = require_('moment');
+	const luxon = require_('luxon');
+
 	const timestampTask = () => gulp.src(src, { base: base })
 		.pipe($.plumber())
 		.pipe($.ignore.include({ isFile: true }))
-		.pipe($.replace(/v\d+t/g, `v${moment()().format('HHmmss')}t`))
+		.pipe($.replace(/v\d+t/g, `v${luxon().DateTime.now().toFormat('HHmmss')}t`))
 		.pipe($.changed(src, { hasChanged: $.changed.compareContents }))
 		.pipe(gulp.dest(dest));
 	return timestampTask;
