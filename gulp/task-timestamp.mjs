72ffc93f5639694e46857129ac491827559968bf
Takuto Yanagida <yanagida@stxst.com>
2024-04-19 10:22:58 +0900

    Update packages, gulp tasks, and docs

--- gulp/task-timestamp.mjs
+++ gulp/task-timestamp.mjs
@@ -2,14 +2,14 @@
  * Function for gulp (Timestamp)
  *
  * @author Takuto Yanagida
- * @version 2022-09-16
+ * @version 2023-11-08
  */
 
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import ignore from 'gulp-ignore';
 import replace from 'gulp-replace';
-import changed from 'gulp-changed';
+import changed, { compareContents } from 'gulp-changed';
 import { DateTime } from 'luxon';
 
 export function makeTimestampTask(src, dest = './dist', base = null) {
@@ -17,7 +17,7 @@ export function makeTimestampTask(src, dest = './dist', base = null) {
 		.pipe(plumber())
 		.pipe(ignore.include({ isFile: true }))
 		.pipe(replace(/v\d+t/g, `v${DateTime.now().toFormat('HHmmss')}t`))
-		.pipe(changed(src, { hasChanged: changed.compareContents }))
+		.pipe(changed(src, { hasChanged: compareContents }))
 		.pipe(gulp.dest(dest));
 	return timestampTask;
 }
